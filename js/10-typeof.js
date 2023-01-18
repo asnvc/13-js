788895a901b2b813c03dc90a0f4fb5c0d344733a
xzcjdvd <deividas.asanaviciu@gmail.com>
2023-01-18 11:36:41 +0200

    typeof, array.isarray

---
+++ js/10-typeof.js
@@ -0,0 +1,25 @@
+console.clear()
+/*
+TYPEOF (tipo nustatymas)
+operatorius
+*/
+
+console.log(typeof 5);
+console.log(typeof Infinity);
+console.log(typeof NaN);
+console.log(typeof -3.14);
+
+console.log(typeof '');
+console.log(typeof 'earsgf');
+
+console.log(typeof true);
+console.log(typeof false);
+
+console.log(typeof function () {});
+
+console.log(typeof []);
+console.log(typeof [5]);
+console.log(typeof ['asdasdas']);
+console.log(typeof [true]);
+
+console.log(typeof undefined);
